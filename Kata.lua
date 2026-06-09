b1f015dec7cef1ee3449d74a5d199d4ddc4630ca
Bieoidungbuonnua <166198515+Bieoidungbuonnua@users.noreply.github.com>
2026-06-09 11:26:27 +0700

    Update Kata.lua

--- Kata.lua
+++ Kata.lua
@@ -68,7 +68,7 @@ isHopping = false
 -- Auto rejoin khi bị lỗi / security kick
 local function RejoinSelf()
     pcall(function()
-        RS:WaitForChild("__ServerBrowser"):InvokeServer("teleport", game.JobId)
+        game:GetService("TeleportService"):Teleport(game.PlaceId, LP)
     end)
 end
 game:GetService("CoreGui").RobloxPromptGui.promptOverlay.ChildAdded:Connect(function(child)
