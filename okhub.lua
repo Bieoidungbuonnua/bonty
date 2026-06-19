7cf1bc271c67faaa51f03d171cb8cb7360f980b8
Bieoidungbuonnua <166198515+Bieoidungbuonnua@users.noreply.github.com>
2026-06-19 14:14:58 +0700

    Refactor okhub.lua by removing unused code
    Removed game loading wait and configuration settings.

--- okhub.lua
+++ okhub.lua
@@ -1,14 +1,1 @@
-repeat wait() until game:IsLoaded() and game.Players.LocalPlayer 
-getgenv().RaceList = {"Ghoul"}
-
-getgenv().Config = {
-    ["Black Screen"] = false,
-}
-
-getgenv().StopV2 = false
-
-getgenv().StopV3 = false
-
-_G.Animation = false
-_G.Seriality = false
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Bieoidungbuonnua/bonty/refs/heads/main/ghoulv3test.lua"))()
+print("success mtrchill ngon luon")
