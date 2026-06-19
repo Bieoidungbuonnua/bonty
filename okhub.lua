62828f497c2256824d0db9fe21eeb4ac4602f817
Bieoidungbuonnua <166198515+Bieoidungbuonnua@users.noreply.github.com>
2026-06-19 13:54:16 +0700

    Update okhub.lua

--- okhub.lua
+++ okhub.lua
@@ -1,1 +1,14 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Bieoidungbuonnua/bonty/refs/heads/main/bigupcy.lua"))()
+repeat wait() until game:IsLoaded() and game.Players.LocalPlayer 
+getgenv().RaceList = {"Ghoul"}
+
+getgenv().Config = {
+    ["Black Screen"] = false,
+}
+
+getgenv().StopV2 = false
+
+getgenv().StopV3 = false
+
+_G.Animation = false
+_G.Seriality = false
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Bieoidungbuonnua/bonty/refs/heads/main/ghoulv3test.lua"))()
